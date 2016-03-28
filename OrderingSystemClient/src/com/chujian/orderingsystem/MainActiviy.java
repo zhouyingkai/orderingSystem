04c070b24ca1ce6fbd71bb93bb5126692ecd2cbd
zhouyingkai <893984129@qq.com>
2016-03-28 15:23:53 +0800

    order system client
    first commit

---
+++ OrderingSystemClient/src/com/chujian/orderingsystem/MainActiviy.java
@@ -0,0 +1,13 @@
+package com.chujian.orderingsystem;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActiviy extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activiy_main);
+	}
+
+}
